159684fd4b5522477d2e5d0da8daa47027b88ab9
growtoups <67888122+growtoups@users.noreply.github.com>
2023-08-23 19:17:03 -0400

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,20 +8,22 @@ import Tab from '@mui/material/Tab';
 
 export default function Navbar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ background: "transparent" }}
-elevation={0}>
+    <Box sx={{ flexGrow: 0 }}>
+      <AppBar position="static" sx={{ background: "transparent" }} elevation={0}>
         <Toolbar>
           <img
             src="https://cdn.mycelium-ai.com/image.png"
-            width={50}
-            height={50}
             style={{
+              width: "100%", // Image will resize to the screen width
+              height: "auto", // Maintain aspect ratio
               borderRadius: "50%",
               marginLeft: "auto",
+              marginRight: "auto", // Center the image
+              display: "block", // Remove extra space below image
             }}
           />
           <Typography variant="h5" component="div" sx={{
+            fontSize: "clamp(1rem, 4vw, 2rem)", // Responsive font size
             marginLeft: "1%",
             marginRight: "auto",
             fontWeight: "bolder"
