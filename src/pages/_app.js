ef8c5a6957ab5bef33d7c2ca1f853f6bdfb39799
growtoups <67888122+growtoups@users.noreply.github.com>
2023-09-04 13:11:43 -0400

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }) {
         <meta name="description" content="Welcome to DiscreetShare, your trusted platform for anonymous and free file uploads. With our service, you can confidently share your files while ensuring your privacy remains intact. We've taken steps to prevent any tracebacks and diligently delete all potentially identifying information, giving you the peace of mind you deserve. Experience confidential file sharing today with DiscreetShare!" />
         <meta name="theme-color" content="#fff" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="https://media.discordapp.net/attachments/854578580381892619/1143945141451755530/shushing-face_1f92b.jpg?width=200&height=200" />
+        <link rel="icon" href="https://cdn.mycelium-ai.com/image.png" />
         <meta name="twitter:title" content="DiscreetShare" />
         <meta name="twitter:description" content="Welcome to DiscreetShare, your trusted platform for anonymous and free file uploads. With our service, you can confidently share your files while ensuring your privacy remains intact. We've taken steps to prevent any tracebacks and diligently delete all potentially identifying information, giving you the peace of mind you deserve. Experience confidential file sharing today with DiscreetShare!" />
         <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
