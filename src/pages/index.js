9e5395b56cd8fd3c82125906721ad416558e6708
growtoups <67888122+growtoups@users.noreply.github.com>
2023-10-02 17:49:10 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,87 +5,93 @@ import Box from '@mui/material/Box';
 import axios from 'axios';
 import { toast, ToastContainer } from 'react-toastify'; // Make sure you import 'ToastContainer' too
 import 'react-toastify/dist/ReactToastify.css'; // Import the CSS
+
 const FileUpload = () => {
     const [uploadProgress, setUploadProgress] = useState(0);
     const [downloadLink, setDownloadLink] = useState(null);
-    const [downloadLinks, setDownloadLinks] = useState([]);
     const [error, setError] = useState(false);
+
     const handleFileChange = (event) => {
         const fileInput = event.target;
         const file = fileInput.files[0];
-        const formData = new FormData();
-        formData.append('file', file);
-        axios.post('https://api.discreetshare.com/upload', formData, {
-            onUploadProgress: (progressEvent) => {
-                const percentCompleted = Math.round(
-                    (progressEvent.loaded * 100) / progressEvent.total
-                );
-                setUploadProgress(percentCompleted);
-            },
-        })
-        .then((response) => {
-            if (response.data.status === "size") {
-                toast.error('Your file size is bigger than 1 GB!', { // Use 'toast.error'
-                    position: "bottom-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "dark",
-                });
+        
+        if (file) {
+            const formData = new FormData();
+            formData.append('file', file);
+            
+            axios.post('https://api.discreetshare.com/upload', formData, {
+                onUploadProgress: (progressEvent) => {
+                    const percentCompleted = Math.round(
+                        (progressEvent.loaded * 100) / progressEvent.total
+                    );
+                    setUploadProgress(percentCompleted);
+                },
+            })
+            .then((response) => {
+                if (response.data.status === "size") {
+                    toast.error('Your file size is bigger than 1 GB!', {
+                        position: "bottom-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "dark",
+                    });
+                    setError(true); // Set error to true
+                } else if (response.data.status === "true") {
+                    toast.success('Successfully uploaded your file!', {
+                        position: "bottom-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "dark",
+                    });
+                    setDownloadLink(response.data.downloadLink);
+                } else if (response.data.status === false) {
+                    toast.error('An error happened, please try again later', {
+                        position: "bottom-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "dark",
+                    });
+                    setError(true); // Set error to true
+                } else {
+                    setDownloadLink(response.data.downloadLink);
+                }
+                fileInput.value = '';
+            })
+            .catch((error) => {
+                console.error("An error occurred:", error);
                 setError(true); // Set error to true
-            } else if (response.data.status === "true") {
-                toast.success('Successfully uploaded your file!', {
-                    position: "bottom-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "dark",
-                });
-                setDownloadLinks((prevLinks) => [...prevLinks, response.data.downloadLink]);
-
-            } else if (response.data.status === false) {
-                toast.error('An error happened, please try again later', { // Use 'toast.error'
-                    position: "bottom-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "dark",
-                });
-                setError(true); // Set error to true
-            } else {
-                setDownloadLinks((prevLinks) => [...prevLinks, response.data.downloadLink]);
-
-            }
-            fileInput.value = '';
-        })
-        .catch((error) => {
-            console.error("An error occurred:", error);
-            setError(true); // Set error to true
-        });
+            });
+        }
     };
-const handleCopyClick = () => {
-    const linksToCopy = downloadLinks.join('\n'); // Separate links with newline character
-    navigator.clipboard.writeText(linksToCopy);
-    toast.success('Download links copied!', {
-        position: "bottom-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-    });
-};
+
+    const handleCopyClick = () => {
+        if (downloadLink) {
+            navigator.clipboard.writeText(downloadLink);
+            toast.success('Download link copied!', {
+                position: "bottom-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+        }
+    };
+
     return (
         <>
             <h2>Anonymous File Upload</h2>
@@ -103,7 +109,6 @@ const handleCopyClick = () => {
                         <input
                             style={{ display: 'none' }}
                             id="raised-button-file"
-                            multiple
                             type="file"
                             onChange={handleFileChange}
                         />
@@ -123,17 +128,17 @@ const handleCopyClick = () => {
                         )}
                     </>
                 )}
-{downloadLinks.length > 0 ? (
-    <Button variant="contained" onClick={handleCopyClick} style={{
-        width: "15rem",
-        border: "2px solid #fff",
-        color: "#fff",
-        background: "transparent",
-        fontWeight: "700"
-    }}>
-    <i className='bx bxs-copy'></i>&nbsp;Copy
-    </Button>
-) : null}
+                {downloadLink && (
+                    <Button variant="contained" onClick={handleCopyClick} style={{
+                        width: "15rem",
+                        border: "2px solid #fff",
+                        color: "#fff",
+                        background: "transparent",
+                        fontWeight: "700"
+                    }}>
+                    <i className='bx bxs-copy'></i>&nbsp;Copy
+                    </Button>
+                )}
                 {error && (
                     <Button variant="contained" style={{
                         width: "15rem",
@@ -169,4 +174,5 @@ const handleCopyClick = () => {
         </>
     );
 };
+
 export default FileUpload;
