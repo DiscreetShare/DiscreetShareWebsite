bee4238ab714f9bd50c7ab412fb7bdcc7782a997
growtoups <67888122+growtoups@users.noreply.github.com>
2023-10-10 15:49:58 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -210,7 +210,7 @@ const FileUpload = () => {
                         className='hovera'
                         style={{
                             color: "grey",
-                        }}>Submit Abuse</a> Join us on <a href="https://discord.gg/xDd3V8JHhQ"
+                        }}>Submit Abuse</a> Join us on <a href="https://discord.gg/fB2CG4V5CX"
                             className='hovera'
                             style={{
                                 color: "grey",
