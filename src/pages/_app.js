13090ba4cb3b96138fb92d37f9b36ff8c9199f64
Nathan Laviolette <67888122+growtoups@users.noreply.github.com>
2024-02-08 19:02:18 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }) {
         <meta name="description" content="Welcome to DiscreetShare, your trusted platform for anonymous and free file uploads. With our service, you can confidently share your files while ensuring your privacy remains intact. We've taken steps to prevent any tracebacks and diligently delete all potentially identifying information, giving you the peace of mind you deserve." />
         <meta name="theme-color" content="#fff" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="https://cdn.mycelium-ai.com/image.png" />
+        <link rel="icon" href="https://api.discreetshare.com/cdn/65c55e0952319df41011cd60" />
         <meta name="twitter:title" content="DiscreetShare" />
         <meta name="twitter:description" content="Welcome to DiscreetShare, your trusted platform for anonymous and free file uploads. With our service, you can confidently share your files while ensuring your privacy remains intact. We've taken steps to prevent any tracebacks and diligently delete all potentially identifying information, giving you the peace of mind you deserve." />
         <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
