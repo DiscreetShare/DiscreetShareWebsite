ded127eb5d7010d509a75c86ceebbc5346fa8eec
Nathan Laviolette <67888122+growtoups@users.noreply.github.com>
2024-03-25 11:16:55 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,6 +50,10 @@ const FileUpload = () => {
                 if (status === "true") {
                     showToast('Successfully uploaded your file!');
                     setDownloadLink(downloadLink);
+                    if (status == "ud-403) {
+                        showToast('Sorry the uploads are disabled for now please wait till the maintenance ends', true);
+                    setError(true);
+                        }
                 } else {
                     showToast('An error happened, please try again later', true);
                     setError(true);
