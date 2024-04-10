d7e7b002322cc9d0ff4258d9b11a02e7c1cedbce
Nathan Laviolette <67888122+growtoups@users.noreply.github.com>
2024-04-10 10:21:14 -0400

    Create serviceWorker.js

---
+++ src/pages/serviceWorker.js
@@ -0,0 +1,21 @@
+// serviceWorker.js
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('app-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/index.js',
+        
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
